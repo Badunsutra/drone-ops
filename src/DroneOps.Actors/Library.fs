cab2f4f0eb7886ef5293298fc922f4fe22312dc8
dponomarev <ponomarev.dy@yandex.ru>
2026-05-20 22:06:52 +0300

    Project skeleton

---
+++ src/DroneOps.Actors/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace DroneOps.Actors
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ src/DroneOps.Api/Program.fs
@@ -0,0 +1,20 @@
+module Program
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+[<EntryPoint>]
+let main args =
+    let builder = WebApplication.CreateBuilder(args)
+
+    builder.Services.AddControllers() |> ignore
+
+    let app = builder.Build()
+
+    app.UseHttpsRedirection() |> ignore
+    app.MapControllers() |> ignore
+
+    app.Run()
+
+    0
